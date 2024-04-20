b583f6b0167012685158b269762a930e5aa66e18
Nafie Asfour <ph.nafee.asfour@gmail.com>
2024-04-20 23:43:38 +0300

    Auth step1

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,10 @@
 "use client"
 import styles from "./login.module.css";
 import React from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { authenticateUser } from "@/api/auth";
+
 
 export default function page() {
   return (
@@ -8,7 +12,8 @@ export default function page() {
       <form className={styles.loginform}>
         <input className={styles.logininput} type='text' placeholder="name"/>
         <input className={styles.logininput} type='text' placeholder="password"/>
-        <button className={styles.loginbutton}>login</button>
+        <button className={styles.loginbutton}>Login</button>
+        <button className={styles.loginbutton}>Create New Account</button>
       </form>
     </div>
   )
